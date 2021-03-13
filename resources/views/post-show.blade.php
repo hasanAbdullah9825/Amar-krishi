50f2d1f6dc3b72c50b99d78f4bfa18c5322f9c9f
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-03-13 21:08:06 +0600

    Update

--- resources/views/post-show.blade.php
+++ resources/views/post-show.blade.php
@@ -72,7 +72,8 @@
          <div class="media mt-4">
           <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
           <div class="media-body">
-            <h5 class="mt-0">{{$comment->user->name}}</h5>
+            <h5 class="mt-0">
+              {{$comment->user->name}}</h5>
           {{$comment->content}}
           </div>
         </div>
